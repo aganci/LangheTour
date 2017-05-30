e862c36be4fb4e57d52513d2f4079af50425e410
aganci <aganci@gmail.com>
2017-05-30 11:17:52 +0200

    add styled toolbar

--- app/src/main/java/com/langhetour/MainActivity.java
+++ app/src/main/java/com/langhetour/MainActivity.java
@@ -3,6 +3,7 @@ package com.langhetour;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(getResources().getString(R.string.app_name));
 
         new GridBuilder((ConstraintLayout) findViewById(R.id.main_constraint_layout), this)
                 .addCategory("Visitare")
