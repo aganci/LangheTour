ef8c89223e40dbdf197f42c6cefa82a14cdb16d0
aganci <aganci@gmail.com>
2017-06-02 16:19:59 +0200

    add map activity

--- app/src/main/java/com/langhetour/MainActivity.java
+++ app/src/main/java/com/langhetour/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.langhetour.models.TourItem;
@@ -18,10 +20,34 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(getResources().getString(R.string.app_name));
+        setSupportActionBar(toolbar);
 
         GridBuilder gridBuilder = new GridBuilder((ConstraintLayout) findViewById(R.id.main_constraint_layout), this);
         for (TourItem item : TourItem.ITEMS) {
             item.addTo(gridBuilder, this);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_show_map:
+                showMap();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    private void showMap() {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
     }
 }
